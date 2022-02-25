342a0d407ab13637047bde08a36f44ad06e61904
LucaKrause1 <lucakrause00@gmail.com>
2022-02-25 18:45:16 +0100

    Schlaeger bewegen sich richtig -> nur noch groesse und Umwandlung aendern. Sieben Segment-Anzeige gibt die Punkte der Spieler aus, flackert aber, wenn das LCD-Display aktualisiert wird.

--- Pong.asm
+++ Pong.asm
@@ -21,7 +21,9 @@
 
 VarSection	Section
 			org $0040
-;speicher ds.b 8
+PositionAVorher ds.b 1
+PositionBVorher	ds.b 1
+change 			ds.b 1
 
 ; -----------------------------------------------------------------------------
 ;   Beginn des Programmcodes im schreibgeschuetzten Teil des Speichers
@@ -36,18 +38,61 @@ RomSection	Section
 Start	
 			lds #$3FFF		;Stack-Pointer initialisieren
 			
-			jsr initAll			
+			jsr initAll		
+			
+			ldd #1234		;Beispiel-Punkte
+			std PunkteA
+			ldd #5678
+			std PunkteB	
+			
+			jsr pointsToString
+			jsr pointsToSegments
+			
+			ldaa #0
+			staa PositionAVorher
+			staa PositionBVorher
+			
+SchlaegerDarstellen
+			;Schlaeger in A und B
+			jsr startADC
+			staa BatPos1
+			stab BatPos2
+	
+			;konvertiere Schlaeger
+			jsr transformCoords			;transformierte Koordinaten in BatPos1 und BatPos2
+			
+			ldaa BatPos1
+			staa PositionAVorher
+			ldaa BatPos2
+			staa PositionBVorher
+			
+			jsr setBats
 						
 Play	
 			;Schlaeger in A und B
 			jsr startADC
 			staa BatPos1
 			stab BatPos2
+	
 			;konvertiere Schlaeger
 			jsr transformCoords			;transformierte Koordinaten in BatPos1 und BatPos2
 			
-			jsr setBats
+			ldaa BatPos1
+			ldab BatPos2
+			cmpa PositionAVorher
+			bne ShowNewPosOfBats		 
+Same		
+			cmpb PositionBVorher
+			beq Play
+
+ShowNewPosOfBats
+			ldaa BatPos1
+			staa PositionAVorher
+			ldaa BatPos2
+			staa PositionBVorher
 			
+			jsr setBats
+
 			bra Play
 			
 Ende
@@ -62,8 +107,8 @@ Ende
 			include Potis.inc
 			include init.inc
 			include Ball.inc
-			;include Debug.inc
-			;include Ein_Ausgabe.inc
+			include Debug.inc
+			include Ein_Ausgabe.inc
 			
 					
 			include includes\LCDfont.inc		;TODO: an richtiger Stelle?
@@ -89,17 +134,17 @@ VecSPI			dc.w Error				; $FFD8 SPI Serial Transfer Complete
 VecPAI			dc.w Error				; $FFDA Pulse Accumulator Input Edge
 VecPAO			dc.w Error				; $FFDC Pulse Accumulator Overflow
 VecTOF			dc.w Error				; $FFDE Timer Overflow
-VecTOC5		dc.w Error				; $FFE0 Timer Output Compare 5
-VecTOC4		dc.w Error				; $FFE2 Timer Output Compare 4
-VecTOC3		dc.w Error				; $FFE4 Timer Output Compare 3
-VecTOC2		dc.w Error				; $FFE6 Timer Output Compare 2
-VecTOC1		dc.w isrOC1			; $FFE8 Timer Output Compare 1
-VecTIC3		dc.w Error				; $FFEA Timer Input Capture 3
-VecTIC2		dc.w Error				; $FFEC Timer Input Capture 2
-VecTIC1		dc.w Error				; $FFEE Timer Input Capture 1
+VecTOC5			dc.w Error				; $FFE0 Timer Output Compare 5
+VecTOC4			dc.w Error				; $FFE2 Timer Output Compare 4
+VecTOC3			dc.w Error				; $FFE4 Timer Output Compare 3
+VecTOC2			dc.w Error				; $FFE6 Timer Output Compare 2
+VecTOC1			dc.w isrOC1				; $FFE8 Timer Output Compare 1
+VecTIC3			dc.w Error				; $FFEA Timer Input Capture 3
+VecTIC2			dc.w Error				; $FFEC Timer Input Capture 2
+VecTIC1			dc.w Error				; $FFEE Timer Input Capture 1
 VecRTI			dc.w Error				; $FFF0 Real Time Interrupt
 VecIRQ			dc.w Error				; $FFF2 External Interrupt Request
-VecXIRQ		dc.w Error				; $FFF4 Non-Maskable Interrupt
+VecXIRQ			dc.w Error				; $FFF4 Non-Maskable Interrupt
 VecSWI			dc.w Error				; $FFF6 Software Interrupt
 VecIOT			dc.w Error				; $FFF8 Illegal Opcode Trap
 VecCOP			dc.w Error				; $FFFA Computer Operate Properly 
