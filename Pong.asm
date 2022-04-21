4d5626769d81d29a0d02368e8a2f9d36a8f341ca
Luca Krause <lucakrause00@gmail.com>
2022-04-21 22:32:16 +0200

    Format geändert mit Leerzeichen.

--- Pong.asm
+++ Pong.asm
@@ -49,7 +49,7 @@ Play
 			stab BatPos2
 	
 			;konvertiere Schlaeger
-			jsr transformCoords			;transformierte Koordinaten in BatPos1 und BatPos2
+			jsr transformCoords             ;transformierte Koordinaten in BatPos1 und BatPos2
 			
 			jsr setBats
 			
@@ -58,7 +58,7 @@ Play
 			
 			;Tastenueberpruefung
 			ldaa PIO_B
-			anda #%00000001			;Spiel zuruecksetzen, falls Taste "0" gedrueckt	
+			anda #%00000001                 ;Spiel zuruecksetzen, falls Taste "0" gedrueckt	
 			
 			beq Reset					
 
@@ -70,7 +70,7 @@ Play
 ;	nutzt A,B
 ; -----------------------------------------------------------------------------
 Reset
-			sei									;Interrupts unterbinden, da keine Ballbewegung mehr erlaubt ist 
+			sei                             ;Interrupts unterbinden, da keine Ballbewegung mehr erlaubt ist 
 													
 			ldaa BallX
 			ldab BallY
@@ -98,30 +98,30 @@ Reset
 ;   Vektortabelle
 ; -----------------------------------------------------------------------------
 
-Error		bra	*								; hier waere Fehlerbehandlung sinnvoll
+Error		bra	*                           ; hier waere Fehlerbehandlung sinnvoll
 
 ;*** Vektoren ***
 Vector		section
 			org	$FFD6
 
-VecSCI				dc.w Error				; $FFD6 SCI Serial System
-VecSPI				dc.w Error				; $FFD8 SPI Serial Transfer Complete
-VecPAI				dc.w Error				; $FFDA Pulse Accumulator Input Edge
-VecPAO				dc.w Error				; $FFDC Pulse Accumulator Overflow
-VecTOF				dc.w Error				; $FFDE Timer Overflow
-VecTOC5			dc.w Error				; $FFE0 Timer Output Compare 5
-VecTOC4			dc.w Error				; $FFE2 Timer Output Compare 4
-VecTOC3			dc.w Error				; $FFE4 Timer Output Compare 3
-VecTOC2			dc.w Error			; $FFE6 Timer Output Compare 2
-VecTOC1			dc.w isrOC1			; $FFE8 Timer Output Compare 1
-VecTIC3			dc.w Error				; $FFEA Timer Input Capture 3
-VecTIC2			dc.w Error				; $FFEC Timer Input Capture 2
-VecTIC1			dc.w Error				; $FFEE Timer Input Capture 1
-VecRTI				dc.w Error				; $FFF0 Real Time Interrupt
-VecIRQ				dc.w Error				; $FFF2 External Interrupt Request
-VecXIRQ			dc.w Error				; $FFF4 Non-Maskable Interrupt
-VecSWI				dc.w Error				; $FFF6 Software Interrupt
-VecIOT				dc.w Error				; $FFF8 Illegal Opcode Trap
-VecCOP				dc.w Error				; $FFFA Computer Operate Properly 
-VecCLK				dc.w Error				; $FFFC Clock Monitor Reset
-VecRESET			dc.w Start				; $FFFE Reset
+VecSCI          dc.w Error              ; $FFD6 SCI Serial System
+VecSPI          dc.w Error              ; $FFD8 SPI Serial Transfer Complete
+VecPAI          dc.w Error              ; $FFDA Pulse Accumulator Input Edge
+VecPAO          dc.w Error              ; $FFDC Pulse Accumulator Overflow
+VecTOF          dc.w Error              ; $FFDE Timer Overflow
+VecTOC5         dc.w Error              ; $FFE0 Timer Output Compare 5
+VecTOC4         dc.w Error              ; $FFE2 Timer Output Compare 4
+VecTOC3         dc.w Error              ; $FFE4 Timer Output Compare 3
+VecTOC2         dc.w Error              ; $FFE6 Timer Output Compare 2
+VecTOC1         dc.w isrOC1             ; $FFE8 Timer Output Compare 1
+VecTIC3         dc.w Error              ; $FFEA Timer Input Capture 3
+VecTIC2         dc.w Error              ; $FFEC Timer Input Capture 2
+VecTIC1         dc.w Error              ; $FFEE Timer Input Capture 1
+VecRTI          dc.w Error              ; $FFF0 Real Time Interrupt
+VecIRQ          dc.w Error              ; $FFF2 External Interrupt Request
+VecXIRQ         dc.w Error              ; $FFF4 Non-Maskable Interrupt
+VecSWI          dc.w Error              ; $FFF6 Software Interrupt
+VecIOT          dc.w Error              ; $FFF8 Illegal Opcode Trap
+VecCOP          dc.w Error              ; $FFFA Computer Operate Properly 
+VecCLK          dc.w Error              ; $FFFC Clock Monitor Reset
+VecRESET        dc.w Start              ; $FFFE Reset
